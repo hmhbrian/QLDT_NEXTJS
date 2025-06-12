8144296cf84c7948e7a41b9f96a000f9fc2a6ae1
L1-Loki <hungnguyen2003k@gmail.com>
2025-06-12 12:51:28 +0700

    chore: update project configuration and add eslint settings

---
+++ src/config/eslint-disable.js
@@ -0,0 +1,11 @@
+ 
+ 
+ 
+ 
+
+// This file exists to disable specific ESLint rules for specified files in the codebase
+// Add comments about why rules are disabled when necessary
+
+// Example:
+// For UI components, we sometimes need to use 'any' type for dynamic props:
+// /* eslint-disable @typescript-eslint/no-explicit-any */ 
