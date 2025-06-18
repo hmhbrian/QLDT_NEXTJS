9622b8c6a7726201ed749f7252fce0ffb7b0ead7
L1-Loki <hungnguyen2003k@gmail.com>
2025-06-18 08:10:24 +0700

    chore: update next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -11,23 +11,23 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['images.unsplash.com', 'placehold.co'],
+    domains: ["images.unsplash.com", "placehold.co"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://192.168.20.3:5000/api/:path*', // Proxy tới server API thực tế
+        source: "/api/:path*",
+        destination: "http://localhost:5228/api/:path*", // Proxy tới server API thực tế với /api prefix
       },
-    ]
+    ];
   },
 };
 
