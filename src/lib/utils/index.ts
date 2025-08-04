1807f178bae998ace664037e0084026f58e6ae44
L1-Loki <hungnguyen2003k@gmail.com>
2025-08-04 11:54:36 +0700

    refactor(utils): Refactor utility functions and API mappers

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -7,11 +7,14 @@
 // Core utilities
 export { cn } from "../utils";
 
+// Cookie management
+export { cookieManager, CookieManager } from "./cookie-manager";
+export type { CookieOptions, CookieInfo } from "./cookie-manager";
+
 // Domain-specific utilities
 export * from "./form.utils";
-export * from "./string.utils"; 
+export * from "./string.utils";
 export * from "./date.utils";
-export * from "./error.utils";
 export * from "./performance.utils";
 export * from "./department-tree";
 export * from "./code-generator";
